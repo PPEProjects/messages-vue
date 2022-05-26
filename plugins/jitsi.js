94af523e10119c1e4a1549ef0069f666ff2cb58b
coder <dnstylish@gmail.com>
2022-05-26 16:16:20 +0700

    add call note

--- plugins/jitsi.js
+++ plugins/jitsi.js
@@ -19,7 +19,7 @@ export default ({ app, store }, inject) => {
 
       const left = (screen.width - popupWinWidth) / 2;
       const top = (screen.height - popupWinHeight) / 4;
-      const newWindow = window.open('http://localhost:3000/groupcall?room=' + id , 'Smileeye Call','resizable=yes, width=' + popupWinWidth + ', height=' + popupWinHeight + ', top=' + top + ', left=' + left);
+      const newWindow = window.open('https://chat1.smileeye.edu.vn/groupcall?room=' + id , 'Smileeye Call','resizable=yes, width=' + popupWinWidth + ', height=' + popupWinHeight + ', top=' + top + ', left=' + left);
       if (newWindow.onbeforeunload) {
         // bị huỷ mở cửa sổ
         onCancel()
